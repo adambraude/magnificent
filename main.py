38cfbfaaed52d2d944d95354ab3b9070076556ca
piscythe <malfunctioningadamantium@gmail.com>
2019-05-01 13:03:54 -0700

    first real commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print ("Hello, and welcome to Splandor!")
+
+colors = "W", "U", "G", "R", "B"
+
+# color, points, white, blue, green, red, black
+tier1Deck = [
+(0, 0, 1, 1, 1, 1, 0),
+(0, 0, 1, 2, 1, 1, 0),
+(0, 0, 2, 2, 0, 1, 0),
+(0, 0, 0, 0, 1, 3, 1),
+(0, 0, 0, 0, 2, 1, 0),
+(0, 0, 2, 0, 2, 0, 0),
+(0, 0, 0, 0, 3, 0, 0),
+(0, 1, 0, 4, 0, 0, 0)
+    ];
+
+print ("Tier 1 deck initialized with", len(tier1Deck), "cards")
